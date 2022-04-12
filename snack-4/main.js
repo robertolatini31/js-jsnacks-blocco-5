8f85b587628ce2a21ca4328238386da8601b9389
robertolatini31 <robertolatini31@gmail.com>
2022-04-12 12:42:41 +0200

    added with forEach

--- snack-4/main.js
+++ snack-4/main.js
@@ -6,14 +6,26 @@
 const primoArray = ['a', 'b', 'c'];
 const secondoArray = [1,2,3];
 
+// function fusionArray (primoArray, secondoArray) {
+//     let arrayFusion = [];
+//     for(let i = 0; i < primoArray.length; i++){
+//         arrayFusion.push(primoArray[i]);
+       
+//         arrayFusion.push(secondoArray[i]);
+//     }
+//     return arrayFusion;
+// }
+
 function fusionArray (primoArray, secondoArray) {
     let arrayFusion = [];
-    for(let i = 0; i < primoArray.length; i++){
-        arrayFusion.push(primoArray[i]);
-       
-        arrayFusion.push(secondoArray[i]);
-    }
+    primoArray.forEach((element, i) => {
+        arrayFusion.push(element);
+        arrayFusion.push(secondoArray[i])
+    });
     return arrayFusion;
 }
+
+
+
 
 console.log(fusionArray(primoArray, secondoArray));
