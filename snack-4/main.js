4adeaf582a7b7a7082ed9332e8da7ccc47dd2f7d
robertolatini31 <robertolatini31@gmail.com>
2022-04-12 12:31:45 +0200

    added snack-4

---
+++ snack-4/main.js
@@ -0,0 +1,19 @@
+// SNACK 4
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi)
+// prendendo alternativamente gli elementi da uno e dall’altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const primoArray = ['a', 'b', 'c'];
+const secondoArray = [1,2,3];
+
+function fusionArray (primoArray, secondoArray) {
+    let arrayFusion = [];
+    for(let i = 0; i < primoArray.length; i++){
+        arrayFusion.push(primoArray[i]);
+       
+        arrayFusion.push(secondoArray[i]);
+    }
+    return arrayFusion;
+}
+
+console.log(fusionArray(primoArray, secondoArray));
